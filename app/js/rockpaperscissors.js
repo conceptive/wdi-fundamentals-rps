2ec5511fc9dbc202dfc561bd300c6bf547a6653a
Frank Juan <frank@conceptivestudio.com>
2015-02-23 02:11:48 -0500

    Update rockpaperscissors.js

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -50,10 +50,17 @@ function getWinner(playerMove,computerMove) {
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+
+    var scores = {player: 0, computer: 0,tie: 0};
+    while (playerWins < 5 && computerWins < 5) {
+        var playerMove   = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner       = getWinner(playerMove, computerMove);
+        scores[winner] += 1;
+
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+        console.log("The score is currently --- Player: " + playerWins + " vs Computer: " + computerWins);
+    }
     return [playerWins, computerWins];
 }
 
